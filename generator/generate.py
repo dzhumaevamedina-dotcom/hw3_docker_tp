b9e3794bc674489dcc2a835a87b9b3360cae2092
dzhumaevamedina-dotcom <dzhumaeva_medina@mail.ru>
2026-06-13 02:53:12 +0300

    изменены названия колонок

--- generator/generate.py
+++ generator/generate.py
@@ -5,16 +5,15 @@ import sys
 
 NUM_ROWS = 50
 
-
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["CLASS_ID", "TIME", "VISITORS", "ROOM"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "CLASS_ID": random.randint(0,100),
+        "TIME": round(random.uniform(1.5, 9.9), 2),
+        "VISITORS": random.randint(0,100),
+        "ROOM": random.choice(["A", "B", "C"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
