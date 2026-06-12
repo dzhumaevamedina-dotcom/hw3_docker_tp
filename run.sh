c3091fe27f2c999b470796bfd979b637bca8f954
dzhumaevamedina-dotcom <dzhumaeva_medina@mail.ru>
2026-06-13 01:18:06 +0300

    добавлен контейнер аналитика

--- run.sh
+++ run.sh
@@ -3,11 +3,17 @@
 cmd=$1
 
 if [ "$cmd" = "build_generator" ]; then
-    docker build -t hw3_gen ./generator
+  docker build -t hw3_gen ./generator
 
 elif [ "$cmd" = "run_generator" ]; then
-    docker run --rm -v "$(pwd)/data:/data" hw3_gen
+  docker run --rm -v "$(pwd)/data:/data" hw3_gen
+
+elif [ "$cmd" = "build_reporter" ]; then
+  docker build -t hw3_rep ./reporter
+
+elif [ "$cmd" = "run_reporter" ]; then
+  docker run --rm -v "$(pwd)/data:/data" hw3_rep
 
 else
-    echo "Unknown command"
+  echo "Unknown command"
 fi
